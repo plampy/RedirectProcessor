685d9d60d1197c23f90e50b42a74821d40bd00a7
Kelly Olsen <plampy@gmail.com>
2019-02-16 14:51:06 -0700

    Scaffold library. Create interface, WIP implementation

---
+++ RedirectProcessor/Interface/IRouteAnalyzer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RedirectProcessor.Interface
+{
+    public interface IRouteAnalyzer
+    {
+        IEnumerable<string> Process(IEnumerable<string> routes);
+    }
+}
---
+++ RedirectProcessor/RouteAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿using RedirectProcessor.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace RedirectProcessor
+{
+    public class RouteAnalyzer : IRouteAnalyzer
+    {
+        public IEnumerable<string> Process(IEnumerable<string> routes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
