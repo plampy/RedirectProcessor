799c95f020e0a45adeb7a5ed557ac469264745d7
Kelly Olsen <plampy@gmail.com>
2019-02-16 14:59:53 -0700

    Add test project + console app

---
+++ RedirectProcessor.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RedirectProcessor.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new List<string>
+            {
+                "/home",
+                "/one.html -> /two.html",
+                "/two.html -> /three.html",
+                "/three.html -> /home"
+            };
+
+            var analyzer = new RouteAnalyzer();
+            Console.WriteLine("Processing...");
+            input.ForEach(Console.WriteLine);
+            var results = analyzer.Process(input).ToList();
+            Console.WriteLine("Resulting paths:");
+            results.ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RedirectProcessor.Test/RouteAnalyzerTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace RedirectProcessor.Test
+{
+    [TestClass]
+    public class RouteAnalyzerTests
+    {
+        [TestMethod]
+        public void Process_SampleInput_MatchesOutput()
+        {
+            var input = new string[]
+            {
+                "/home",
+                "/our-ceo.html -> /about-us.html",
+                "/about-us.html -> /about",
+                "/product-1.html -> /seo"
+            };
+            var analyzer = new RouteAnalyzer();
+            var result = analyzer.Process(input).ToList();
+
+            var expected = new[]
+            {
+                "/home",
+                "/product-1.html -> /seo",
+                "/our-ceo.html -> /about-us.html -> /about",
+            };
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+
+        [TestMethod]
+        public void Process_SingleCircularRedirectPath_ThrowsExceptionWithMessage()
+        {
+            var failureInput = new string[]
+            {
+                "/about-us.html -> /about",
+                "/about -> /about-us.html"
+            };
+            var analyzer = new RouteAnalyzer();
+
+            try
+            {
+                var result = analyzer.Process(failureInput).ToList();
+                Assert.Fail("Expected to throw");
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.Message.Contains("Circular route found"));
+            }
+        }
+    }
+}
